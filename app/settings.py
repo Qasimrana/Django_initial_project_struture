e705273e7d6ba1bf4d9a27334bc94e823c00bbda
TDLP057\Qasim Ishtiaq <mqasimrana08@gmail.com>
2023-01-11 15:26:41 +0500

    postgresql connection with django

--- app/settings.py
+++ app/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -88,8 +89,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qr_db', 
+        'USER': 'postgres', 
+        'PASSWORD': '',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
